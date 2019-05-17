eecad4f483fb8862f4d6be3f8dadb4b8f47020d2
hemesh <hemeshreddy@gmail.com>
2019-05-18 01:11:53 +1000

    second commit

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/hemeshreddys/Desktop/Projects/Gatsby/gatsbyProject/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/hemeshreddys/Desktop/Projects/Gatsby/gatsbyProject/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/hemeshreddys/Desktop/Projects/Gatsby/gatsbyProject/.cache/data.json")
+
